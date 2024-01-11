0bc09031ae6100c04576a41fc7cfd89a2066d90a
Abdul Ali Noori <ab.noori.1993@gmail.com>
2024-01-11 11:51:04 +0330

    Add the routes.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://127.0.0.1:5500"
 
     resource "*",
       headers: :any,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-# config/routes.rb
 Rails.application.routes.draw do
   namespace :api do
-    resources :searches, only: %i[index show create]
+    resources :articles, only: [:create, :index]
+    resources :searches, only: [:index, :show, :create]
   end
 end
