fe5d36a1d5d66154f161f8e6dcd8a4e831c550f5
Abdul Ali Noori <ab.noori.1993@gmail.com>
2024-01-11 11:50:14 +0330

    Add the search_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_09_201039) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_161121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "searches", force: :cascade do |t|
     t.string "query"
